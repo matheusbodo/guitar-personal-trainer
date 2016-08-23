5f0e68bae88e39b41d5e045f8e0b4ae419d1605d
Matheus Bodo <mcbodo@gmail.com>
2016-08-22 21:50:59 -0300

    Initial commit. Creating spring boot project

---
+++ src/main/java/br/com/matheusbodo/gpt/GuitarPersonalTrainerApplication.java
@@ -0,0 +1,12 @@
+package br.com.matheusbodo.gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarPersonalTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarPersonalTrainerApplication.class, args);
+	}
+}
